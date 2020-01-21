dc0125f16aa714f4d591cc9d1cb90ac40278fbde
lvpeng990324 <lvpeng990324@qq.com>
2020-01-21 11:44:50 +0800

    初步定义admin文件

--- performance/admin.py
+++ performance/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import MonthlyPerformance
+from .models import MonthlySalesData
+from .models import QuarterlyPerformance
+from .models import InternalControlIndicators
+from .models import QuarterlySalesData
 
-# Register your models here.
+
+@admin.register(MonthlyPerformance)
+class MonthlyPerformanceInformation(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MonthlySalesData)
+class MonthlySalesDataInformation(admin.ModelAdmin):
+    pass
+
+
+@admin.register(QuarterlyPerformance)
+class QuarterlyPerformanceInformation(admin.ModelAdmin):
+    pass
+
+
+@admin.register(InternalControlIndicators)
+class InternalControlIndicatorsInformation(admin.ModelAdmin):
+    pass
+
+
+@admin.register(QuarterlySalesData)
+class QuarterlySalesDataInformation(admin.ModelAdmin):
+    pass
+
+
+
