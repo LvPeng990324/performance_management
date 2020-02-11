fcaf30789da73c6e191485f6207ea7718988b845
shenyexin <47884039+shenyexin@users.noreply.github.com>
2020-02-11 17:14:17 +0800

    创建用户扩展模型

--- performance/models.py
+++ performance/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+# 用户扩展表
+class UserExtension(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='extension')
+    job_number = models.CharField(max_length=100, verbose_name='工号')
+    telephone = models.CharField(max_length=100, verbose_name='电话号码')
+    department = models.CharField(max_length=100, verbose_name='部门')
+
+# 接收到save信号，将扩展表与原表绑定
+@receiver(post_save, sender=User)
+def handler_user_extension(sender, instance, created, **kwargs):
+    if created:
+        UserExtension.objects.create(user=instance)
+    else:
+        instance.extension.save()
 
 
 # 月度营业数据
