daa37d95edc3c97902fb3e8613faf79aa4880f36
lvpeng990324 <lvpeng990324@qq.com>
2020-02-15 08:34:51 +0800

    页面显示已登陆用户名

--- performance_management/settings.py
+++ performance_management/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'performance_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
