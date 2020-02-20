748da6be5505c1bc7bdf9e63088e20641ea01af5
huangjiac <60732172+huangjiac@users.noreply.github.com>
2020-02-20 17:51:11 +0800

    添加了月度表中数据被删除的情况

--- performance/utils/CalculateQuarterlySalesData.py
+++ performance/utils/CalculateQuarterlySalesData.py
@@ -14,6 +14,7 @@ from django.db.models import Sum
 def quarterly_data_production(year, year_res_set, quarter_list):
     """
     创建季度数据并存入数据库
+    PS.消息队列可能需要依赖返回值
     :param year: 年份
     :param year_res_set: 全年数据
     :param quarter_list: 季度列表（{(1,2,3),(4,5,6),(7,,8,9),(10,11,12)}）
@@ -27,31 +28,39 @@ def quarterly_data_production(year, year_res_set, quarter_list):
     if 10 in quarter_list:
         quarter = 4
 
-    res = year_res_set.filter(month__in=quarter_list).aggregate(
-        turnover_sum=Sum("turnover"),
-        operating_expenses_sum=Sum("operating_expenses"),
-        amount_repaid_sum=Sum("amount_repaid"),
-        inventory_sum=Sum("inventory"),
-        profit_sum=Sum("profit")
-    )
-
-    new_data = {
-        'year': year,
-        'quarter': quarter,
-        'turnover': res['turnover_sum'],
-        'operating_expenses': res['operating_expenses_sum'],
-        'amount_repaid': res['amount_repaid_sum'],
-        'inventory': res['inventory_sum'],
-        'profit': res['profit_sum'],
-    }
-    obj = QuarterlySalesData.objects.filter(year=year, quarter=quarter)
-    if obj:
-        # 如果该月数据已存在，则更新
-        QuarterlySalesData.objects.filter(year=year, quarter=quarter).update(**new_data)
-        # print("%s年%s季 更新成功" % (year, quarter))
+    res = year_res_set.filter(month__in=quarter_list)
+    if res.exists():
+        # 月度表中存在数据
+        res.aggregate(
+            turnover_sum=Sum("turnover"),
+            operating_expenses_sum=Sum("operating_expenses"),
+            amount_repaid_sum=Sum("amount_repaid"),
+            inventory_sum=Sum("inventory"),
+            profit_sum=Sum("profit")
+        )
+        new_data = {
+            'year': year,
+            'quarter': quarter,
+            'turnover': res['turnover_sum'],
+            'operating_expenses': res['operating_expenses_sum'],
+            'amount_repaid': res['amount_repaid_sum'],
+            'inventory': res['inventory_sum'],
+            'profit': res['profit_sum'],
+        }
+        obj = QuarterlySalesData.objects.filter(year=year, quarter=quarter)
+        if obj.exists():
+            # 如果该月数据已存在，则更新
+            obj.update(**new_data)
+            # print("%s年%s季 更新成功" % (year, quarter))
+        else:
+            QuarterlySalesData.objects.create(**new_data)
+            # print("%s年%s季 成功存入" % (year, quarter))
     else:
-        QuarterlySalesData.objects.create(**new_data)
-        # print("%s年%s季 成功存入" % (year, quarter))
+        # 月度表中不存在数据
+        # 同步删除季度表中的数据
+        obj = QuarterlySalesData.objects.filter(year=year, quarter=quarter)
+        if obj.exists():
+            obj.delete()
 
 
 def calculate_quarterly_sales_data(year=MonthlySalesData.objects.values_list('year', flat=True).distinct()):
@@ -65,13 +74,6 @@ def calculate_quarterly_sales_data(year=MonthlySalesData.objects.values_list('ye
             amount_repaid = models.FloatField(verbose_name='回款额')
             inventory = models.FloatField(verbose_name='库存量')
             profit = models.FloatField(verbose_name='利润额')
-
-        致LV：
-        1.消息提示完全没写（所以完全没有返回值）
-        2.错误捕获也没写
-        3.就算缺数据也会求和（比如第一季度只有前两个月的数据）
-        4.主函数是这个
-        5.(2020/2/20)默认全部求和，可传入列表求指定的年份
     """
 
     success_message = ""
