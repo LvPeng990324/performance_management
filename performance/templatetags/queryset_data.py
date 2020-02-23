eafa73a29603ba826b312031e7b1da212ab0f34c
lvpeng990324 <lvpeng990324@qq.com>
2020-02-23 22:12:51 +0800

    修改内控表当前时间与订单时间相同时状态判断出错的问题

--- performance/templatetags/queryset_data.py
+++ performance/templatetags/queryset_data.py
@@ -42,7 +42,7 @@ def progress(order_date, scheduled_delivery):
     current_date = datetime.date.today()
     # 如果当前时间已经超过计划交期，返回False
     if current_date > scheduled_delivery:
-        return False
+        return -1
     # 计算百分数
     return ((current_date - order_date).days / (scheduled_delivery - order_date).days)*100
 
