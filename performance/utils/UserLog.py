cd5d74b345d3d4d8e608b1f89c2853f338f5e5b4
lvpeng990324 <lvpeng990324@qq.com>
2020-03-02 13:02:31 +0800

    增加参数说明

--- performance/utils/UserLog.py
+++ performance/utils/UserLog.py
@@ -4,6 +4,10 @@ from ..models import Logs
 
 # 增加日志
 def add_log(request, action, result):
+    # 参数说明
+    # request：就是视图那个request
+    # action：记录用户行为，str类型
+    # result：用户动作是否成功，str类型
     user_name = request.user.last_name
     job_number = request.user.extension.job_number
     # 记录到数据库
