407dec26bb5b7b3018edcbcea55c645bc99050b6
lvpeng990324 <lvpeng990324@qq.com>
2020-03-04 09:32:58 +0800

    增加公告表

--- performance/models.py
+++ performance/models.py
@@ -227,3 +227,18 @@ class Logs(models.Model):
 
     def __str__(self):
         return '{} {}'.format(str(self.user_name), str(self.action))
+
+
+# 公告表
+class Announcement(models.Model):
+    time = models.DateTimeField(verbose_name='时间')
+    title = models.CharField(max_length=200, verbose_name='标题')
+    content = models.TextField(verbose_name='内容')
+    who = models.CharField(max_length=100, verbose_name='发布者')
+
+    class Meta:
+        verbose_name_plural = '公告表'
+        verbose_name = '公告表'
+
+    def __str__(self):
+        return self.title
