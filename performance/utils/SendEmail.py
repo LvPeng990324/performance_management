b8799a26d5433ffc91b9b74760bf1d2173919c06
lvpeng990324 <lvpeng990324@qq.com>
2020-03-14 22:05:04 +0800

    增加发送邮箱验证码方法

--- performance/utils/SendEmail.py
+++ performance/utils/SendEmail.py
@@ -21,3 +21,20 @@ def send_backup(file_path):
     msg.attach_file(file_path)
     # 发送邮件
     msg.send()
+
+
+# 发送邮箱验证方法
+def send_verification_code(to_email, verification_code):
+    # 两个参数，要发送的邮箱地址以及验证码
+    # 从配置文件读取发送邮箱
+    from_email = settings.EMAIL_HOST_USER
+    # 邮件标题
+    subject = '利润绩效系统验证码'
+    # 邮件正文
+    context = '欢迎使用绩效管理系统，你的验证码为{}，请不要回复此邮件。'.format(verification_code)
+    # 从数据库中取得要发送的email
+    to_email = SystemConfig.objects.first().backup_to_email
+    # 创建邮件对象
+    msg = EmailMultiAlternatives(subject, context, from_email, [to_email])
+    # 发送邮件
+    msg.send()
